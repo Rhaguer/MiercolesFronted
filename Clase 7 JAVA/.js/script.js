e9c0b8ade5e00e76486dbffc236f99803c5045c0
Rhaguer <ricardo.haguer@inacapmail.cl>
2024-06-26 19:30:04 -0400

    JAVA

--- Clase 7 JAVA/.js/script.js
+++ Clase 7 JAVA/.js/script.js
@@ -4,3 +4,53 @@
 var nombre= "francisco";
 let edad = 34;
 const esDocente = true; 
+
+//TIPO DE DATOS
+let numero = 400;
+let texto = "ESTOY EN INACAP";
+let booleano = true;
+let indefinido;
+
+//OPERADORES
+let sumaa = 5 + 5;
+let restaa = 5 - 5;
+let concatenacion = "HOLA ESTOY" + " " + "en inacap";//HOLA ESTOY EN INACAP
+
+let numero1 = 10;
+let numero2 = 5;
+
+
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multi = numero1 * numero2;
+let divi = numero1 / numero2;
+
+console.log(suma);
+console.log(resta);
+console.log(multi);
+console.log(divi);
+
+let texto1 = "Hola";
+let texto2 = "ESTOY EB INACAP"
+let texto3 = 
+
+let texto3 = texo1+ " " texto2;
+
+//ESTRUCTURA DE CONTROL 
+//IF
+let dias = 30;
+if (dias <=20){
+    console.log("estoy en vacaciones");
+} else {
+    console.log("estoy en clse")
+}
+
+//BUCLE
+for (let i=0; i < 5; i++){
+    console.log(1numero: +contador);
+
+}
+        
+
+
+
